be43d4fc5dd1abfb845701771ba7216bb346b52d
mariana-pineda <139593142+mariana-pineda@users.noreply.github.com>
2024-08-14 15:00:49 -0500

    Update and rename dbo.Test.Table.sql to dbo.Orders.Table.sql

---
+++ Pubs/table/dbo.Orders.Table.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+    [OrderId] [int] IDENTITY(1,1) NOT NULL,
+    [Code] [nvarchar](max) NULL,
+    [Date] [datetime] NOT NULL,
+    [Type] [nvarchar](max) NULL,
+    [ItemName] [nvarchar](max) NULL,
+    [Status] [nvarchar](max) NULL,
+    [Total] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_dbo.Orders] PRIMARY KEY CLUSTERED 
+(
+    [OrderId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
